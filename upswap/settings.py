041d279b3fff20a5eb19557b678f1593f91b1562
Yashofficial20 <yashuqwer7@gmail.com>
2024-11-13 11:51:51 +0530

    Commit Changes

--- upswap/settings.py
+++ upswap/settings.py
@@ -103,7 +103,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://api.upswap.app",
+    # other allowed origins
+]
 
 # Optional: Allow credentials (cookies, authorization headers)
 CORS_ALLOW_CREDENTIALS = True
