7377f946242611b1c941edd19e3ccee1d5c41511
Yashofficial20 <yashuqwer7@gmail.com>
2024-12-02 13:32:07 +0530

    Commit Changes

--- main/serializers.py
+++ main/serializers.py
@@ -565,7 +565,7 @@ class CreateDealSerializer(serializers.ModelSerializer):
     discount_percentage = serializers.SerializerMethodField()
     uploaded_images = serializers.ListField(
         child=serializers.DictField(
-            child=serializers.CharField(max_length=255),
+            child=serializers.CharField(max_length=10000),
             required=True,
         ),
         required=False,
