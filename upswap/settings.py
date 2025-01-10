3bb794942d8545ff3c1761491d8c646d29d94265
Yashofficial20 <yashuqwer7@gmail.com>
2025-01-10 15:47:00 +0530

    Commit Changes

--- upswap/settings.py
+++ upswap/settings.py
@@ -247,7 +247,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=6),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
