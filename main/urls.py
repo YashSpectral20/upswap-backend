89dde847b6756768907ad99825dc52b5fd1302f1
Yashofficial20 <yashuqwer7@gmail.com>
2025-03-28 12:20:25 +0530

    Commit Changes

--- main/urls.py
+++ main/urls.py
@@ -202,7 +202,7 @@ urlpatterns = [
     
     # PlaceOrder
     path('place-order/', PlaceOrderView.as_view(), name='place-order'),
-    path('place-order/details/<uuid:order_id>/', PlaceOrderDetailsView.as_view(), name='place-order-details'),
+    path('place-order/details/<uuid:placeorder_id>/', PlaceOrderDetailsView.as_view(), name='place-order-details'),
     path('place-order/lists/', PlaceOrderListsView.as_view(), name='place-order-lists'),
     
     path('categories/', CategoriesView.as_view(), name='categories'),
@@ -230,7 +230,7 @@ urlpatterns = [
     
     path('submit-rating/<uuid:order_id>/', SubmitRatingView.as_view(), name='submit-rating'),
     
-    path("place-order/<uuid:placeorder_id>/raise-issue/", RaiseAnIssueMyOrdersView.as_view(), name="raise-issue"),
+    path("place-order/<uuid:place_order_id>/raise-issue/", RaiseAnIssueMyOrdersView.as_view(), name="raise-issue"),
     
     path('raise-issue/vendors/<uuid:vendor_id>/', RaiseAnIssueVendorsCreateView.as_view(), name='raise-issue-vendors'),
     
