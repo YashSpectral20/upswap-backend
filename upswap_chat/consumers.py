2e486db7a9beec7ef77c38c533933983c71f9afd
rahuldey7 <rahul.dey@spectralshifttech.com>
2025-04-08 18:34:13 +0530

    minor fixes 8-4-2025 06:34

--- upswap_chat/consumers.py
+++ upswap_chat/consumers.py
@@ -26,10 +26,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json.get('message', '')
-
+        user = self.scope['user']
         await self.create_message(
             chat_room=self.chat_room,
-            sender=self.scope['user'],
+            sender=user,
             content=message
         )
 
@@ -38,7 +38,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             {
                 'type': 'chat_message',
                 'message': message,
-                'sender': self.scope['user'],
+                'sender': user.username,
                 'sent_at': timezone.now().strftime('%Y-%m-%d %H:%M:%S')
             }
         )
