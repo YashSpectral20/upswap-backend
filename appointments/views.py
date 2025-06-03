21dde0c22702407bfa13011e124b148edfeb3186
rahuldey7 <rahul.dey@spectralshifttech.com>
2025-06-03 13:53:24 +0530

    fix upload pfp on provider API

--- appointments/views.py
+++ appointments/views.py
@@ -2,6 +2,7 @@ from rest_framework import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from main.utils import upload_to_s3
 from .serializers import ProviderSerializer
 
 class ProviderAPIView(APIView):
@@ -41,6 +42,7 @@ class ProviderAPIView(APIView):
     def post(self, request, format=None):
         try:
             user = request.user
+            profile_picture = request.FILES.get('profilePhoto')
             if not user.vendor_kyc:
                 return Response({
                     'message': 'You must be a vendor to add a provider.',
@@ -48,15 +50,22 @@ class ProviderAPIView(APIView):
                 }, status=status.HTTP_401_UNAUTHORIZED)
             serializer = ProviderSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save(vendor=user.vendor_kyc)
+                if profile_picture:
+                    # upload profile picture to s3
+                    profile_picture_url = upload_to_s3(profile_picture, f'profile-pictures/{user.vendor_kyc.id}/', profile_picture.name)
+                    serializer.save(vendor=user.vendor_kyc, profile_picture=[profile_picture_url])
+                else:
+                    serializer.save(vendor=user.vendor_kyc)
                 return Response({
                     'message': 'Provider added successfully.',
                     'data': serializer.data
                 }, status=status.HTTP_201_CREATED)
+            
             return Response({
                 'message': 'Failed to add provider.',
                 'error': serializer.errors
             }, status=status.HTTP_400_BAD_REQUEST)
+        
         except Exception as e:
             return Response({
                 'message': 'An error occurred while adding the provider.',
